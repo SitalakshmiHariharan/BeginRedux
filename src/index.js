132314900cb9876fe4af09547aaa200166b6895e
Veeraraghavan <veeraraghvan@gmail.com>
2021-05-24 00:50:01 -0400

    Just redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,43 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux';
+
+
+//ACTION: The action that we want to do
+const increment =() =>{
+  return{
+    type:'INCREMENT'
+  }
+}
+
+const decrement =() =>{
+  return{
+    type:'DECREMENT'
+  }
+}
+
+//REDUCER: Describes how action transforms the state into another state
+let prevState = 0 // This can be initialized inside the function below as well like (prevState=0,action)
+const counter = (prevState, action) => {
+    switch(action.type){
+      case "INCREMENT":
+        return prevState +1;
+      case "DECREMENT":
+        return prevState -1;
+    } 
+}
+
+//STORE: Globalized state
+let store = createStore(counter);
+
+//Display it in console
+store.subscribe(()=> console.log(store.getState()));  
+
+//DISPATCH: This is how we Execute the action. 
+store.dispatch(increment());
+store.dispatch(decrement());
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
